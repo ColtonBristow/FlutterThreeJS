6c49c8a1dbe679485c6d94ebdfe6160d37c92941
ColtonBristow <95318223+ColtonBristow@users.noreply.github.com>
2022-05-04 16:23:22 -0500

    running off futures and competers now

--- threeJS_Viewer/lib/threeJSController.dart
+++ threeJS_Viewer/lib/threeJSController.dart
@@ -1,27 +1,9 @@
 import 'package:threeJS_Viewer/threeJSModelViewer.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ThreeJSController {
-  Function(String color, double alpha) setBackgroundColor;
-  Function(Vector3 pos) setCameraPosition;
-  Function(Vector3 pos) setCameraRotation;
-  Function(String color, int intensity) addAmbientLight;
-  Function(DirectionalLight light) addDirectionalLight;
-  Function(bool enable) enableZoom;
-  Function(Vector3 pos) setControlsTarget;
-  Function(OrbitControls orbitControls) setOrbitControls;
-  Function(bool enable) setStats;
-  Function(Vector3 vector) tweenCamera;
-
+  WebViewController webController;
   ThreeJSController({
-    required this.tweenCamera,
-    required this.setBackgroundColor,
-    required this.addAmbientLight,
-    required this.setCameraPosition,
-    required this.setCameraRotation,
-    required this.addDirectionalLight,
-    required this.enableZoom,
-    required this.setControlsTarget,
-    required this.setOrbitControls,
-    required this.setStats,
+    required this.webController,
   });
 }
--- threeJS_Viewer/lib/threeJSViewer.dart
+++ threeJS_Viewer/lib/threeJSViewer.dart
@@ -10,29 +10,23 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 // ignore: must_be_immutable
 class ThreeJSViewer extends StatefulWidget {
-  final List<ThreeModel> models;
-  final Function(ThreeJSController controller) onPageLoaded;
-  final PerspectiveCameraConfig cameraConfig;
-  final Duration loaderDuration;
-  final Function(bool ready)? onServerReady;
-  final Function(double? percentage)? onObjectLoading;
-  final Function()? onObjectLoaded;
-  final Function(Object error)? onError;
-  final Widget? showWhenLoading;
-  WebViewController? controller;
+  ThreeJSController? controller;
+  Function? onPageFinishedLoading;
+  Iterable<JavascriptChannel>? channels;
+  Function? onError;
+  PerspectiveCameraConfig? cameraConfig;
+  LocalAssetsServer? addressServer;
+  bool? debug;
 
   ThreeJSViewer({
     Key? key,
-    required this.models,
-    required this.onPageLoaded,
-    required this.cameraConfig,
+    this.cameraConfig,
     this.controller,
-    this.onServerReady,
+    this.channels,
     this.onError,
-    this.onObjectLoaded,
-    this.onObjectLoading,
-    this.showWhenLoading,
-    this.loaderDuration = const Duration(milliseconds: 500),
+    this.onPageFinishedLoading,
+    this.addressServer,
+    this.debug,
   }) : super(key: key);
 
   @override
@@ -40,193 +34,97 @@ class ThreeJSViewer extends StatefulWidget {
 }
 
 class _ThreeJSViewerState extends State<ThreeJSViewer> {
-  bool isListening = false;
-  bool isReady = false;
-  String? address;
-  int? port;
-  bool hasError = false;
-
+  Future<InternetAddress>? address;
+  double loadingProgress = 0;
+  int port = 4000;
   Set<JavascriptChannel> channels = {};
 
-  Future<String?>? setupScene() {
-    if (hasError) {
-      log('error setting up scene');
-      return null;
-    }
-
-    return widget.controller?.runJavascriptReturningResult('setupScene()');
-  }
-
-  void loadModels() {
-    if (hasError) return;
-    for (var model in widget.models) {
-      widget.controller?.runJavascript('window.loadModel(\'${model.src}\', ${model.playAnimation})');
+  initServer() async {
+    if (widget.addressServer == null) {
+      final server = LocalAssetsServer(
+        port: 4000,
+        address: InternetAddress.loopbackIPv4,
+        assetsBasePath: 'packages/threeJS_Viewer/web',
+        logger: const DebugLogger(),
+      );
+      setState(() {});
     }
   }
 
-  Future<String?>? createCamera(PerspectiveCameraConfig cameraConfig) {
-    if (hasError) return null;
-    return widget.controller?.runJavascriptReturningResult('window.createPerspectiveCamera($cameraConfig)');
-  }
-
-  void createOrbitControls() {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.createOrbitControls(undefined)');
-  }
-
-  void setBackgroundColor(String color, double alpha) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.setBackgroundColor(\'$color\', $alpha)');
-  }
-
-  void setCameraPosition(Vector3 pos) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.setCameraPosition($pos)');
-  }
-
-  void setCameraRotation(Vector3 pos) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.setCameraRotation($pos)');
-  }
-
-  void setOrbitControls(OrbitControls orbitControls) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.setOrbitControls($orbitControls)');
-  }
-
-  void addAmbientLight(String color, int intensity) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.addAmbientLight(\'$color\', $intensity)');
-  }
-
-  void addDirectionalLight(DirectionalLight light) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.addDirectionalLight(${light.toString(map: true)})');
-  }
-
-  void setControlsTarget(Vector3 pos) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.setControlsTarget($pos)');
-  }
-
-  void enableZoom(bool enable) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.enableZoom($enable)');
-  }
-
-  void setStats(bool enable) {
-    if (hasError) return;
-    widget.controller?.runJavascript('window.setStats($enable)');
-  }
-
-  void _onObjectLoaded() {
-    if (widget.onObjectLoaded != null) widget.onObjectLoaded!();
-    Timer(widget.loaderDuration, () {
-      setState(() {
-        isReady = true;
-      });
-    });
-  }
-
-  void tweenCamera(Vector3 vector) {
-    widget.controller?.runJavascript("tweenCamera(${vector.toString()})");
-  }
-
-  void _onPageFinishedLoading(_) async {
-    await Future.delayed(const Duration(milliseconds: 100));
-    await setupScene();
-    await createCamera(widget.cameraConfig);
-    createOrbitControls();
-    loadModels();
-    widget.onPageLoaded(
-      ThreeJSController(
-        tweenCamera: tweenCamera,
-        setBackgroundColor: setBackgroundColor,
-        addAmbientLight: addAmbientLight,
-        setCameraPosition: setCameraPosition,
-        setCameraRotation: setCameraRotation,
-        addDirectionalLight: addDirectionalLight,
-        enableZoom: enableZoom,
-        setControlsTarget: setControlsTarget,
-        setOrbitControls: setOrbitControls,
-        setStats: setStats,
+  initChannels() {
+    channels = {
+      JavascriptChannel(
+        name: "Print",
+        onMessageReceived: (JavascriptMessage message) {
+          log("from js: ${message.message}");
+        },
       ),
-    );
+      JavascriptChannel(
+        name: "Debug",
+        onMessageReceived: (JavascriptMessage message) {
+          log("from three js: ${message.message}");
+        },
+      ),
+      JavascriptChannel(
+        name: "PostMessage",
+        onMessageReceived: (JavascriptMessage message) {
+          log("from three js: ${message.message}");
+        },
+      )
+    };
   }
 
   @override
   initState() {
     if (Platform.isAndroid) WebView.platform = AndroidWebView();
 
-    _initChannels();
-    _initServer();
+    initServer();
+    initChannels();
     super.initState();
   }
 
-  _initChannels() {
-    channels = {
-      JavascriptChannel(
-          name: 'OnObjectLoading',
-          onMessageReceived: (message) {
-            double? value = double.tryParse(message.message);
-            if (widget.onObjectLoading != null) widget.onObjectLoading!(value);
-            if (value == 100) {
-              _onObjectLoaded();
-            }
-          }),
-      JavascriptChannel(
-          name: 'Print',
-          onMessageReceived: (message) {
-            log(message.message);
-          })
-    };
-  }
-
-  _initServer() async {
-    if (widget.onServerReady != null) widget.onServerReady!(false);
-    final server = LocalAssetsServer(
-      port: 4000,
-      address: InternetAddress.loopbackIPv4,
-      assetsBasePath: 'packages/threeJS_Viewer/web',
-      logger: const DebugLogger(),
-    );
-
-    final address = await server.serve();
-
-    setState(() {
-      this.address = address.address;
-      port = server.boundPort!;
-      isListening = true;
-    });
-    if (widget.onServerReady != null) widget.onServerReady!(true);
+  @override
+  dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    log('now listening on http://$address:$port');
-    return isListening
-        ? Stack(
-            children: [
-              if (!isReady) widget.showWhenLoading ?? const SizedBox(),
-              WebView(
-                debuggingEnabled: true,
-                backgroundColor: Colors.transparent,
-                initialUrl: 'http://$address:$port',
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (c) {
-                  widget.controller = c;
-                },
-                onPageFinished: _onPageFinishedLoading,
-                javascriptChannels: channels,
-                onWebResourceError: (error) {
-                  hasError = true;
-                  if (widget.onError != null) {
-                    widget.onError!(error.description);
-                  }
-                },
-              ),
-            ],
-          )
-        : const SizedBox();
+    var wcontroller;
+    return FutureBuilder(
+      future: address,
+      builder: (context, snapshot) {
+        if (snapshot.hasData == false) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          InternetAddress address = snapshot.data as InternetAddress;
+          log('started local server http://${address.address}:$port');
+          return WebView(
+            debuggingEnabled: true,
+            backgroundColor: Colors.transparent,
+            initialUrl: 'http://${address.address}:$port',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (c) {
+              setState(() {
+                wcontroller = c;
+              });
+              widget.controller = ThreeJSController(webController: c);
+            },
+            onPageFinished: (details) {
+              widget.onPageFinishedLoading;
+              log("calling js");
+              var setupResults = wcontroller.runJavascriptReturningResult('window.setupScene(${widget.debug ?? false})');
+            },
+            javascriptChannels: channels,
+            onWebResourceError: (error) {
+              widget.onError;
+            },
+          );
+        }
+      },
+    );
   }
 }
